5ab506214e10c008866edb51b3d1f41da9bc7948
mclakov <40365971+mclakov@users.noreply.github.com>
2022-02-23 23:16:36 +0300

    feat: function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let str = n.toString();
+    let arr = [];
+    for(let i = 0; i<=str.length; i++) {
+        if(str[i] != "-") {
+            arr.push(str[i]);
+        }
+    };
+    return Number(arr.reverse().join(''));
 }
